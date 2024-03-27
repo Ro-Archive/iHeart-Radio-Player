8276bbfb8e21fc3a97176a1110a94638d6bed98d
GhostyTongue <50715350+Ghosty-Tongue@users.noreply.github.com>
2024-03-27 10:55:39 -0700

    Update Get_Radio_Info.php

--- Get_Radio_Info.php
+++ Get_Radio_Info.php
@@ -6,7 +6,6 @@ if (isset($_POST['radio_name'])) {
     // Construct the API URL with the provided radio name
     $apiUrl = 'https://ro-archive.xyz/iHeartAPI/?name=' . urlencode($radioName);
 
-    // Use cURL to make the API request
     $ch = curl_init($apiUrl);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     $apiResponse = curl_exec($ch);
@@ -15,7 +14,6 @@ if (isset($_POST['radio_name'])) {
     // Decode the JSON response
     $responseData = json_decode($apiResponse, true);
 
-    // Check if the response is successful
     if ($responseData && $responseData['success']) {
         $secureShoutcastStream = getSecureShoutcastStreamUrl($responseData);
         $playlistUrl = getPlaylistUrl($responseData);
@@ -26,10 +24,8 @@ if (isset($_POST['radio_name'])) {
         $html .= 'Your browser does not support the audio tag.';
         $html .= '</audio>';
 
-        // Display track information from HLS playlist dynamically
         $html .= '<div id="track-info"></div>';
 
-        // Add a script to dynamically load track information from HLS playlist
         $html .= '<script>';
         $html .= 'var playlistUrl = "' . $playlistUrl . '";';
         $html .= 'loadTrackInfo(playlistUrl);';
@@ -38,12 +34,10 @@ if (isset($_POST['radio_name'])) {
         echo $html;
     } else {
         echo "Error retrieving radio information.";
-        // Handle the error as needed
     }
 }
 
 function getSecureShoutcastStreamUrl($responseData) {
-    // Check if the response contains stream URLs
     if (isset($responseData['stream_urls']) && is_array($responseData['stream_urls'])) {
         // Loop through stream URLs to find the secure Shoutcast stream
         foreach ($responseData['stream_urls'] as $stream) {
@@ -53,12 +47,10 @@ function getSecureShoutcastStreamUrl($responseData) {
         }
     }
 
-    // If no secure Shoutcast stream URL is found, return an empty string or handle as needed
     return '';
 }
 
 function getPlaylistUrl($responseData) {
-    // Check if the response contains stream URLs
     if (isset($responseData['stream_urls']) && is_array($responseData['stream_urls'])) {
         // Loop through stream URLs to find the playlist URL
         foreach ($responseData['stream_urls'] as $stream) {
@@ -68,7 +60,6 @@ function getPlaylistUrl($responseData) {
         }
     }
 
-    // If no playlist URL is found, return an empty string or handle as needed
     return '';
 }
 ?>
